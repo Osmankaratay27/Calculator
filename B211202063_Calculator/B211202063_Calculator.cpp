39e31f5cf81c3eaac27355698745c090c83b4d41
Osmankaratay27 <osmankaratay754@gmail.com>
2023-01-05 20:00:07 +0300

    Proje dosyası ekle.

---
+++ B211202063_Calculator/B211202063_Calculator.cpp
@@ -0,0 +1,11 @@
+﻿// B211202063_Calculator.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
